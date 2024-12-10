47a678201b59a26f0e018581f38808c298e40675
LeNNdy <vasilkovarsenij3@gmail.com>
2024-12-10 18:13:10 +0300

    are_back2

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import telebot
 from telebot import types
 # importing os module for environment variables
 import os
+from calculate_calories import calculate_calories
+
 import sys 
 sys.stdout.reconfigure(encoding='utf-8')
 
@@ -22,41 +24,6 @@ bot = telebot.TeleBot(TOKEN)
 
 # Хранилище данных пользователей
 user_data = {}
-
-def calculate_calories(data):
-    #Рассчитывает калорийность и БЖУ
-    weight = data["weight"]
-    height = data["height"]
-    age = data["age"]
-    gender = data["gender"]
-    goal = data["goal"]
-
-    # Основной обмен веществ (формула Миффлина-Сан Жеора)
-    if gender == "Мужской":
-        bmr = 10 * weight + 6.25 * height - 5 * age + 5
-    else:
-        bmr = 10 * weight + 6.25 * height - 5 * age - 161
-
-    # Калорийность в зависимости от цели
-    if goal == "Похудение":
-        calories = bmr * 0.8  # Уменьшаем калорийность на 20%
-    elif goal == "Набор массы":
-        calories = bmr * 1.2  # Увеличиваем калорийность на 20%
-    else:  # Поддержание веса
-        calories = bmr
-
-    # Расчет БЖУ (примерные пропорции)
-    protein = round(0.3 * calories / 4, 2)  # 30% калорий из белков
-    fat = round(0.25 * calories / 9, 2)     # 25% калорий из жиров
-    carbs = round(0.45 * calories / 4, 2)   # 45% калорий из углеводов
-
-    return {
-        "calories": round(calories, 2),
-        "protein": protein,
-        "fat": fat,
-        "carbs": carbs
-    }
-
 
 
 @bot.message_handler(commands=["start"])
